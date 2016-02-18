70277664ccf1c009dba1f35d746a7dcb2bcc07a3
Julson Lim <julson@Camden.local>
2016-02-17 17:21:07 -0800

    Added weather check

--- project.clj
+++ project.clj
@@ -4,10 +4,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-json "0.4.0"]
+                 [clj-http "2.1.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler merlin.handler/app}
   :source-paths ["src/clj"]
+  :test-paths ["test/clj"]
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- src/clj/merlin/handler.clj
+++ src/clj/merlin/handler.clj
@@ -1,11 +1,19 @@
 (ns merlin.handler
-  (:require [compojure.core :refer :all]
+  (:require [clj-http.client :as client]
+            [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]))
+
+(defn weather []
+  (let [url "http://api.openweathermap.org/data/2.5/weather?q=san_francisco,ca&appid=67e7bf3c28af8c8707d707b23a9296b7"]
+    (get-in (client/get url {:as :json-strict}) [:body :main])))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] {:headers {"Content-Type" "application/json;charset=utf-8"}
+               :body (weather)})
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (wrap-defaults app-routes site-defaults)
+      wrap-json-response))
