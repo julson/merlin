ccef2d966c8bd88c4fb0c61ea3a0a7dca9ee057d
Julson Lim <julsonlim@gmail.com>
2016-07-04 17:36:59 -0700

    Fixed project file

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
 (defproject merlin "0.1.0-SNAPSHOT"
-  :description "Making you aware of the stuff around you"
+  :description "FIXME"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
+                 [compojure "1.5.0"]
+                 [ring "1.4.0"]
+                 [ring-server "0.4.0"]
+                 [ring/ring-defaults "0.2.0"]
                  [ring/ring-json "0.4.0"]
                  [clj-http "2.1.0"]]
+
   :plugins [[lein-ring "0.9.7"]]
+
   :ring {:handler merlin.handler/app}
+
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
+
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
